4a48c7432dfafbd464da5a52c4c88d9091965ead
AnimalXing <66161458+AnimalXing@users.noreply.github.com>
2022-05-21 11:57:06 -0400

    Create cost_tool.php

---
+++ cost_tool.php
@@ -0,0 +1,27 @@
+add_filter( 'woocommerce_distance_rate_shipping_rule_cost_distance_shipping', function( $rule_cost, $rule, $distance, $package ) {
+	$order_total = $package['contents_cost'];
+	if ( $distance <= 15 ) {
+		$rule_cost = 0;
+	}
+	if ( $distance > 15 ) {
+		$rule_cost = 50;
+	}
+	if ( $order_total > 1000 && $distance > 15 ) {
+		$rule_cost = 0;
+	}
+	
+	if ( $distance > 30 ) {
+		$rule_cost = 100;
+	}
+	if ( $order_total > 2000 && $distance > 30 ) {
+		$rule_cost = 0;
+	}
+	if ( $distance > 50 && $distance <= 100) {
+		$rule_cost = 200;
+	}
+	if ( $order_total > 4000 && $distance > 50 && $distance <= 100) {
+		$rule_cost = 0;
+	}
+
+  return $rule_cost;
+}, 10, 4 );
